f1c102c85d2743a932e628b2b345a3860dee9736
KaramHassoun1 <mohamedkaram.hassoun@insat.u-carthage.tn>
2023-05-10 11:07:26 +0000

    Add required params to update DynamoDB Table

--- index.js
+++ index.js
@@ -6,40 +6,41 @@ class DynamoDBGSIPlugin {
     constructor(serverless, options) {
         this.serverless = serverless;
         this.options = options;
-        this.provider = this.serverless.getProvider('aws');
 
         this.hooks = {
             'before:package:createDeploymentArtifacts': this.removeGSIsFromTemplate.bind(this),
             'after:deploy:deploy': this.createGSIs.bind(this),
         };
-
         this.gsiIndexes = [];
+        this.attributeDefinitions = [];
     }
 
     removeGSIsFromTemplate() {
         const { resources: { Resources } } = this.serverless.service;
         
         if (Resources) {
-          Object.values(Resources).forEach((resource) => {
-            if (resource.Type === 'AWS::DynamoDB::Table') {
-                const { TableName, GlobalSecondaryIndexes, AttributeDefinitions } = resource.Properties;
-                this.gsiIndexes[TableName] = GlobalSecondaryIndexes;
-                delete resource.Properties.GlobalSecondaryIndexes;
-                GlobalSecondaryIndexes.forEach(({ KeySchema: [{ AttributeName }] }) => {
-                    const index = AttributeDefinitions.findIndex(({ AttributeName: name }) => name === AttributeName);
-                    if (index !== -1) {
-                        AttributeDefinitions.splice(index, 1);
+            for (const resource of Object.values(Resources)) {
+                if (resource.Type === 'AWS::DynamoDB::Table') {
+                    const { TableName, GlobalSecondaryIndexes, AttributeDefinitions } = resource.Properties;
+                    this.gsiIndexes.push({ [TableName]: GlobalSecondaryIndexes });
+                    delete resource.Properties.GlobalSecondaryIndexes;
+                    for (const { KeySchema: [{ AttributeName }] } of GlobalSecondaryIndexes) {
+                        const index = AttributeDefinitions.findIndex(({ AttributeName: name }) => name === AttributeName);
+                        if (index !== -1) {
+                            this.attributeDefinitions.push(AttributeDefinitions.splice(index, 1)[0]);
+                        }
                     }
-                });
+                }
             }
-          });
         }
     }
-
-    createGSIs() {
+    
+    async createGSIs() {
         const region = this.options.region;
         const dynamoDb = new AWS.DynamoDB({ region });
-        for (const [tableName, gsis] of this.gsiIndexes) {
+        for (const [index, obj] of Object.entries(this.gsiIndexes)) {
+            const tableName = Object.keys(obj)[0];
+            const gsis = Object.values(obj)[0];
             for (const gsi of gsis) {
                 const params = {
                     TableName: tableName,
@@ -48,12 +49,12 @@ class DynamoDBGSIPlugin {
                             Create: gsi,
                         },
                     ],
+                    AttributeDefinitions: this.attributeDefinitions,
                 };
                 await dynamoDb.updateTable(params).promise();
             }
         }
     }
 }
-
 
 module.exports = DynamoDBGSIPlugin;
