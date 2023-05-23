47adeb573ee96031cbb9f72b4afe71908c62e468
Henrik Lippke <info@henriklippke.de>
2023-05-23 10:31:20 +0200

    Add CI

--- index.js
+++ index.js
@@ -51,5 +51,44 @@ class DynamoDBGSIPlugin {
     }
 }
 
-
 module.exports = DynamoDBGSIPlugin;
+
+
+/// Compare 
+//bitte schreibe mir eine javascript function die per sdk alle dynamoDB gsi holt und überprüft ob diese mit den per cloudformation definierten GSIs übereinstimmt.
+
+async function checkDynamoDBGSIs(dynamoDb, cloudFormationGsis) {
+    const tableName = "your-table-name";
+  
+    // Get the current GSIs for the DynamoDB table
+    const result = await dynamoDb.describeTable({ TableName: tableName }).promise();
+    const dynamoDbGsis = result.Table.GlobalSecondaryIndexes;
+  
+    // Check if the GSIs in CloudFormation match the ones in DynamoDB
+    let isMatch = true;
+    if (dynamoDbGsis.length !== cloudFormationGsis.length) {
+      isMatch = false;
+    } else {
+      for (let i = 0; i < dynamoDbGsis.length; i++) {
+        const dynamoDbGsi = dynamoDbGsis[i];
+        const cloudFormationGsi = cloudFormationGsis[i];
+        if (dynamoDbGsi.IndexName !== cloudFormationGsi.IndexName ||
+            dynamoDbGsi.KeySchema.length !== cloudFormationGsi.KeySchema.length ||
+            dynamoDbGsi.Projection.ProjectionType !== cloudFormationGsi.ProjectionType) {
+          isMatch = false;
+          break;
+        }
+        for (let j = 0; j < dynamoDbGsi.KeySchema.length; j++) {
+          const dynamoDbKeySchema = dynamoDbGsi.KeySchema[j];
+          const cloudFormationKeySchema = cloudFormationGsi.KeySchema[j];
+          if (dynamoDbKeySchema.AttributeName !== cloudFormationKeySchema.AttributeName ||
+              dynamoDbKeySchema.KeyType !== cloudFormationKeySchema.KeyType) {
+            isMatch = false;
+            break;
+          }
+        }
+      }
+    }
+  
+    return isMatch;
+  }
